1b60a3b75f99770be0aa00175b0ec8231ad7cbe8
Jessica Ho <jessicadinhho@gmail.com>
2022-06-28 14:46:06 -0700

    commit tree e68f82f13c4bd9336fb0470a7ec5aa24daa9d92d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    const drivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return ['Antonia', 'Nuru']
+}
+const returnLastTwoDrivers = function() {
+    return ['Amari', 'Mo']
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+function createFareMultiplier(num1) {
+    return function (num2) {
+        return num1*num2
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers (drivers, fn) {
+    if (fn === returnFirstTwoDrivers) {
+        return ['Antonia', 'Nuru']
+    } else if (fn === returnLastTwoDrivers) {
+        return ['Amari', 'Mo']
+    }
+}
